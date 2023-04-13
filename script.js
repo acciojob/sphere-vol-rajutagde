9b24d1326abc4929051791c273016c3eda1cc5d1
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-13 22:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  const radius = document.getElementById("radius").value;
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume.toFixed(2);
+  return false; // prevent form submission
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
